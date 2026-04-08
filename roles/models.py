451557033c3e7862531833b3bd3e9dbad13cc6ed
Vedukharatmal <vedant14052002@gmail.com>
2026-04-09 00:20:03 +0530

    created tenants and roles models

--- roles/models.py
+++ roles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from common.models import BaseModel
 
-# Create your models here.
+class Role(BaseModel):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- tenants/models.py
+++ tenants/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from common.models import BaseModel
 
-# Create your models here.
+class Tenant(BaseModel):
+    name = models.CharField(max_length=255)
+    subdomain = models.CharField(max_length=100, unique=True)
+    is_active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
